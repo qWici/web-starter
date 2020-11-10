ccd339bfae0aac225f9c3487f2ce8864e4c78e1e
Vania Kucher <dev.kucher@gmail.com>
2020-11-11 00:59:50 +0200

    Revert "Add :: tests"
    This reverts commit c311128c7954cd410fa9ffb8b67c5a0cd0174eae.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 /* Required for build CSS */
 import './scss/style.scss';
-import {Figure} from "./game/figure";
-import {Game} from "./game/game";
-
-const game = new Game(500, 500);
-const block = new Figure('I', game.context);
